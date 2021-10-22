614b65cd81992e4dd1312fd5e58ffb2f6027d2c1
Merfiron <60529978+Merfiron@users.noreply.github.com>
2021-10-22 16:53:59 -0400

    Add files via upload

---
+++ Assets/Scripts/Movement and Gravity/FirstPersonMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public float runSpeed = 9;
+    public KeyCode runningKey = KeyCode.LeftShift;
+
+    Rigidbody rigidbody;
+    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+
+    public Gravity gravity;
+    public ConstantForce customGravity;         // Using this so we're not affecting global gravity
+
+    void Awake()
+    {
+        // Get the rigidbody on this.
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        // Apply custom gravity.
+        customGravity.force = rigidbody.mass * gravity.strength * gravity.direction;
+
+        // Update IsRunning from input.
+        IsRunning = canRun && Input.GetKey(runningKey);
+
+        // Get targetMovingSpeed.
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+        if (speedOverrides.Count > 0)
+        {
+            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+        }
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+
+        // Apply movement.
+        transform.Translate(targetVelocity.x * Time.deltaTime, 0, targetVelocity.y * Time.deltaTime);
+
+    }
+}
