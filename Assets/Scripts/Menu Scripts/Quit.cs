8c1923fd8b52f755a4f001cd2c20b7c696532aff
joshmundell <jmundell@cedarville.edu>
2021-10-29 19:41:56 -0400

    Commit

---
+++ Assets/Scripts/Menu Scripts/Quit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quit : MonoBehaviour
+{
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
