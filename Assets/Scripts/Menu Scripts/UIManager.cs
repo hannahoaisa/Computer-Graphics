cb619a3a6e0b42f9bb558c752d91f1905fb6b2f3
merfiron <thetymerf@Gmail.com>
2021-12-01 22:23:46 -0500

    New level; Eagle introduction
    Added a new level with moderate difficulty that introduces the eagle. Also added an outerspace skybox to each level.

--- Assets/Scripts/Menu Scripts/UIManager.cs
+++ Assets/Scripts/Menu Scripts/UIManager.cs
@@ -168,6 +168,7 @@ public class UIManager : MonoBehaviour
 
     public void nextHandle()
     {
+        Physics.gravity = Vector3.down * erinMove.gravity.strength;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void settingsHandle()
